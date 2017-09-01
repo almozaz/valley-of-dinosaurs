6d6f73de2abec19a1da81a858096ceef9c4a4431
almozaz <alacruzmozaz@gmail.com>
2017-09-01 12:52:59 +0200

    Display a list of dinosaurs. To fix: list repeats itself

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,5 +1,10 @@
 class Dinosaur < ApplicationRecord
-  scope :order_by_name, -> { order(:name) }
+
+  def self.order_by_name
+    order(:name)
+  end
+
+  # scope :order_by_name, -> { order(:name) }
 
   def is_baby?
     age <= 3
